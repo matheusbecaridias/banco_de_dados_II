fe786a6ba5cd19eb0d39f396169c9ef388d7d373
Matheus Becari <matheus.becari.dias@escola.pr.gov.br>
2026-02-11 21:30:01 -0300

    Update ainnn_scriptizinhu.sql

--- ainnn_scriptizinhu.sql
+++ ainnn_scriptizinhu.sql
@@ -1,7 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS Escola;
-
 USE Escola;
-
 CREATE table if not exists Alunos (
 Id INT(10) Primary key auto_increment NOT NULL,   
 Data_de_nascimento Date,
@@ -24,17 +22,21 @@ VALUES ('2006-10-10', 'Leste', 0, 'analista de suporte'),
 
 select * from Alunos;
 
---1. Menores de 18 anos
+#1. Menores de 18 anos
 SELECT * From Alunos WHERE Data_de_nascimento > DATE_SUB(CURRENT_DATE(), INTERVAL 18 YEAR);
---2. Maiores de 18 anos
+#2. Maiores de 18 anos
 SELECT * From Alunos WHERE Data_de_nascimento <= DATE_SUB(CURRENT_DATE(),INTERVAL 18 YEAR);
---3. 
-SELECT * From Alunos WHERE
---4.
-SELECT * From Alunos WHERE
---5.
-SELECT * From Alunos WHERE
---6.
-SELECT * From Alunos WHERE
---7.
-SELECT * From Alunos WHERE
+#3. Veiculo proprio
+SELECT * From Alunos WHERE Veiculo_proprio = 1;
+#4.Apenas região
+SELECT * From Alunos WHERE Veiculo_proprio;
+#5.Trabalham com TI
+SELECT * FROM Alunos WHERE Ocupacao LIKE '%TI';
+#6.Estuda e tem carro proprio
+SELECT * From Alunos WHERE Veiculo_proprio = 1 and Ocupacao = 'Estudante'
+
+#7. A partir da analise dos dados consultados na tabela alunos, é possível notar que: 
+  1. Embora a maioria dos alunos seja maior de idade, apenas dois possuem veículo próprio. Esse dado sugere um recorte social de vulnerabilidade ou desigualdade financeira entre o grupo;
+  2. Contudo, a maioria reside em regiões próximas ao colégio, o que pode indicar que a distância e a dificuldade de locomoção são barreiras para estudantes de zonas mais remotas;
+  3. A baixa atuação atual na área de TI sinaliza uma tendência de transição de carreira ou de busca pelo primeiro ingresso qualificado no mercado através da profissionalização;
+  4. Portanto, o perfil socioeconômico predominante é de adultos que buscam ascensão econômica por meio da inserção no setor tecnológico.
