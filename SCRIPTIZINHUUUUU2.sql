0450b7c3db534eae2e308cf6e666e5fcbd0bd41f
Matheus Becari <matheus.becari.dias@escola.pr.gov.br>
2026-03-23 20:49:22 -0300

    Update SCRIPTIZINHUUUUU2.sql

--- SCRIPTIZINHUUUUU2.sql
+++ SCRIPTIZINHUUUUU2.sql
@@ -6,17 +6,29 @@ Como desejamos os sabores, quantidade e ano, deveremos manipular três tabelas:
 itens_notas_fiscais, tabela_de_produtos e notas_fiscais. 
 */ 
 
-SELECT SABOR AS SABORES FROM tabela_de_produtos TP
-INNER JOIN itens_notas_fiscais INF 
-ON TP.CODIGO_DO_PRODUTO = INF.CODIGO_DO_PRODUTO
-INNER JOIN notas_fiscais NF 
-ON NF.NUMERO = INF.NUMERO
-WHERE YEAR(DATA_VENDA) = 2016;
-
-
-FROM itens_notas_fiscais;
-FROM notas_fiscais;
-FROM tabela_de_produtos;
-
-AS QUANTIDADE(L) ORDER BY DESC;
-AS %_DE_VENDA;
+SELECT 
+    SABORES,
+    QUANTIDADE_ITEM,
+    ANO,
+    QUANTIDADE_TOTAL,
+    (QUANTIDADE_ITEM / QUANTIDADE_TOTAL * 100) AS PERCENTUAL
+FROM (
+    SELECT 
+        TP.SABOR AS SABORES, 
+        SUM(INF.QUANTIDADE) AS QUANTIDADE_ITEM,
+        YEAR(NF.DATA_VENDA) AS ANO,
+        (SELECT SUM(INF.QUANTIDADE) 
+            FROM itens_notas_fiscais INF
+            INNER JOIN notas_fiscais NF
+                ON NF.NUMERO = INF.NUMERO
+            WHERE YEAR(NF.DATA_VENDA) = 2016
+        ) AS QUANTIDADE_TOTAL
+    FROM tabela_de_produtos TP
+    INNER JOIN itens_notas_fiscais INF 
+        ON TP.CODIGO_DO_PRODUTO = INF.CODIGO_DO_PRODUTO
+    INNER JOIN notas_fiscais NF 
+        ON NF.NUMERO = INF.NUMERO
+    WHERE YEAR(NF.DATA_VENDA) = 2016
+    GROUP BY TP.SABOR, YEAR(NF.DATA_VENDA)
+) AS VENDAS_2016
+ORDER BY QUANTIDADE_ITEM DESC;
