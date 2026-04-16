3f5fdbba165cbc7cb88f19665802c57b39a4241e
Matheus Becari <matheus.becari.dias@escola.pr.gov.br>
2026-04-15 22:42:23 -0300

    Add files via upload

---
+++ atividade 4.sql
@@ -0,0 +1,40 @@
+/*CENTRO EST EDU PROF P MARIA R CASTALDI
+Curso Técnico em Análise e Desenvolvimento de Sistemas
+Banco de Dados II
+
+ATIVIDADE 3 - CONSULTAS MYSQL COM JSON 
+
+Baseando-se no banco de dados world_x, realize as seguintes consultas:
+
+/*Consulta 1: Escreva uma consulta que retorne o nome do país e 
+a expectativa de vida de todos os países que pertencem à América do Sul.*/
+
+
+SELECT 
+    JSON_UNQUOTE(JSON_EXTRACT(doc, '$.Name')) AS Nome_Pais,
+    JSON_EXTRACT(doc, '$.demographics.LifeExpectancy') AS Expectativa_Vida
+FROM countryinfo
+WHERE JSON_UNQUOTE(JSON_EXTRACT(doc, '$.geography.Continent')) = 'South America';
+
+
+/*Consulta 2: Escreva uma consulta que liste todos os países onde o Produto Nacional Bruto (GNP) é maior que 500.000 e 
+que possuam o idioma Inglês como um de seus idiomas oficiais.*/
+
+SELECT JSON_UNQUOTE(JSON_EXTRACT(c.data, '$.Name')) AS Nome_Pais
+FROM paises c
+JOIN countrylanguage cl ON JSON_UNQUOTE(JSON_EXTRACT(c.data, '$.Code')) = cl.CountryCode
+WHERE CAST(JSON_UNQUOTE(JSON_EXTRACT(c.data, '$.GNP')) AS DECIMAL(10,2)) > 500000
+  AND cl.Language = 'English'
+  AND cl.IsOfficial = 'T';
+
+/* Agora, a partir do banco de dados empresa, realize as seguintes consultas:
+
+/*Consulta 3: Desenvolva uma consulta que retorne o nome do funcionário e 
+o conteúdo do campo JSON apenas para os funcionários com pelo menos um dependente do sexo Feminino. */
+
+
+
+/*Consulta 4: Utilizando a função JSON_TABLE, retorne todos os dependentes cadastrados no banco de dados. 
+A consulta deve retornar: o nome, parentesco e data de nascimento do dependente e o nome do funcionário. */
+
+
